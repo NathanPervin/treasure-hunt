261e97ed0e64d4baf1d6859b79e1f71f006b0d71
Nathan Pervin <nathanpervin1@gmail.com>
2026-03-12 19:31:54 -0400

    Added other ways to obtain the treasure.

---
+++ treasure_finder.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import requests
+from dotenv import load_dotenv
+
+def main():
+
+    load_dotenv()
+    
+    # Read the secret and repository URL from environment variables
+    TREASURE_KEY = os.environ.get("TREASURE_KEY")
+    REPO_URL = "https://raw.githubusercontent.com/iyad-obeid/secret-message/main/treasure.txt"
+
+    if TREASURE_KEY is None:
+        print("TREASURE_KEY NOT FOUND!")
+        sys.exit(1)
+
+    headers = {
+        "Authorization": f"token {TREASURE_KEY}",
+        "Accept": "application/vnd.github.v3.raw",
+    }
+
+    try:
+        response = requests.get(REPO_URL, headers=headers)
+        response.raise_for_status()  
+        treasure_content = response.text
+        print(treasure_content)
+
+    except Exception as e:
+        print(f"Incorrect Token or HTTP Error: {e}")
+
+if __name__ == "__main__":
+    main()
